ed81a1021d1386ee82c316088b771862442ce8ec
dilietol <dilietol@gmail.com>
2023-12-15 14:15:29 +0100

    MoveVideo Feature: Managing multi destination folders

--- App/MoveVideo.py
+++ App/MoveVideo.py
@@ -7,14 +7,17 @@ from typing import List
 
 VERSION = "0.1"
 
-DIR_IN = "/app/in"
-DIR_OUT = "/app/out"
+DIR_IN = "in"
+DIR_OUT = "out"
+DIR_OUT_ROOT = "."
+DIR_OUT_LABEL = "out"
 
 if not os.path.isdir(DIR_IN):
     raise Exception(f"Directory {DIR_IN} does not exist")
 
 if not os.path.isdir(DIR_OUT):
     raise Exception(f"Directory {DIR_OUT} does not exist")
+
 
 def generate_source_list():
     Item = namedtuple("Item", ["File", "Path", "Key"])
@@ -34,10 +37,10 @@ def generate_source_list():
     return item_list
 
 
-def generate_destination_list():
+def generate_destination_list(dir_out=DIR_OUT):
     Item = namedtuple("Item", ["Dir", "Path", "Key"])
     item_list: List[Item] = list()
-    p = Path(DIR_OUT)
+    p = Path(dir_out)
 
     # Cerca tutte le directory o i symlink presenti nella directory DIR_OUT.
     # Per ogni nome di directory trovato, estrae le chiavi dal nome della directory
@@ -78,42 +81,56 @@ def extract_keys_from_directory_name(file_in):
     return result
 
 
-if __name__ == '__main__':
-    print("Version: " + str(VERSION))
-
-    # Genera la lista dei file sorgente e stampa i nomi dei file e le relative chiavi
+def move_files(dir_out=DIR_OUT):
     source_list = generate_source_list()
+    destination_list = generate_destination_list(dir_out)
+
     source_key_list = [sub.Key for sub in source_list]
     source_filename_list = [sub.File for sub in source_list]
+    destination_dir_list = [sub.Dir for sub in destination_list]
+    destination_key_list = [sub.Key for sub in destination_list]
+
     print("Source filenames:")
     print(source_filename_list)
     print("Source keys:")
     print(source_key_list)
-
-    # Genera la lista delle directory di destinazione e stampa i nomi delle directory e le relative chiavi
-    destination_list = generate_destination_list()
-    destination_dir_list = [sub.Dir for sub in destination_list]
-    destination_key_list = [sub.Key for sub in destination_list]
     print("Destination directories:")
     print(destination_dir_list)
     print("Destination keys:")
     print(destination_key_list)
 
-    # Cerca le chiavi che si trovano sia nella lista sorgente che in quella di destinazione
-    # e stampa le chiavi trovate e quelle mancanti
     found_key_list = list(set(source_key_list) & set(destination_key_list))
     missing_key_list = list(set(source_key_list) - set(destination_key_list))
+
     print("Found keys:")
     print(found_key_list)
     print("Missing keys:")
     print(missing_key_list)
-    sys.stdout.flush()
 
     for key_name in found_key_list:
         destination_dir = [x.Path for x in destination_list if x.Key == key_name]
         source_files = [x.Path for x in source_list if x.Key == key_name]
-        for x in source_files:
-            shutil.move(str(Path(x)), os.path.join(str(Path(destination_dir[0])), os.path.basename(x)))
-            print(r"Moved " + str(Path(x)) + " in " + str(Path(destination_dir[0])))
-            # print(r"Fake moving " + str(Path(x)) + " in " + str(Path(destination_dir[0])))
-            sys.stdout.flush()
+
+        for source_file in source_files:
+            destination_file = os.path.join(str(Path(destination_dir[0])), os.path.basename(source_file))
+            shutil.move(str(Path(source_file)), destination_file)
+            print(f"Moved {source_file} to {destination_file}")
+
+    sys.stdout.flush()
+
+
+def get_subdirectories_with_prefix(directory, prefix):
+    subdirectories = []
+    with os.scandir(directory) as entries:
+        for entry in entries:
+            if entry.is_dir() and entry.name.startswith(prefix):
+                subdirectories.append(entry.path)
+    return subdirectories
+
+
+if __name__ == '__main__':
+    print("Version: " + str(VERSION))
+
+    output_dirs = get_subdirectories_with_prefix(DIR_OUT_ROOT, DIR_OUT_LABEL)
+    for output_dir in output_dirs:
+        move_files(output_dir)
