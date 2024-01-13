b20fffc6022409ce39f93c7c5381311fb7bc16e3
dilietol <dilietol@gmail.com>
2024-01-13 02:35:11 +0100

    Feature - Manage QBittorent

--- App/MoveVideo.py
+++ App/MoveVideo.py
@@ -95,8 +95,8 @@ def move_files(dir_out=DIR_OUT):
     print(source_filename_list)
     print("Source keys:")
     print(source_key_list)
-    print("Destination directories:")
-    print(destination_dir_list)
+    #    print("Destination directories:")
+    #    print(destination_dir_list)
     print("Destination keys:")
     print(destination_key_list)
 
@@ -135,5 +135,3 @@ if __name__ == '__main__':
     output_dirs = get_subdirectories_with_prefix(DIR_OUT_ROOT, DIR_OUT_LABEL)
     for output_dir in output_dirs:
         move_files(output_dir)
-
-
